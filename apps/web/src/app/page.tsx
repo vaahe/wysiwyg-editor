4b7df9890307c1e88a05f46b1e45a8dec7432dff
Vahe <vahee.barseghyan@gmail.com>
2026-02-06 19:18:13 +0400

    fix: critical issues

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@vabanik/editor";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     
+      <Button>
+        sdfsdfgs
+      </Button>
     </div>
   );
 }
